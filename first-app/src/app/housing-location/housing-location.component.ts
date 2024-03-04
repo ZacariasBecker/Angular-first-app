8afe12d2d878de470f74b1740037e8cf89505340
ZacariasBecker <zacariasbecker@gmail.com>
2024-03-04 15:04:39 -0300

    done

--- first-app/src/app/housing-location/housing-location.component.ts
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -2,10 +2,13 @@ import { Component, Input, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
 
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
